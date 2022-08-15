566761893659726e492eaba74bdfe9735a97db37
Arman <arman1231@yandex.ru>
2022-08-15 23:25:10 +0300

    add portfolio

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import AboutMe from './AboutMe/AboutMe';
 import AboutProject from './AboutProject/AboutProject';
 import './Main.css'
+import Portfolio from './Portfolio/Portfolio';
 import Techs from './Techs/Techs';
 
 export default function Main() {
@@ -8,6 +10,8 @@ export default function Main() {
     <main className='main'>
         <AboutProject></AboutProject>
         <Techs></Techs>
+        <AboutMe></AboutMe>
+        <Portfolio></Portfolio>
     </main>
   )
 }
